b479f935fe385ac94111b46cc0f1311eaa2481fd
Esteban Plaza <esteban@vinken.cl>
2021-10-02 16:06:00 -0300

    ADD:  final project Usach

--- src/components/Education.vue
+++ src/components/Education.vue
@@ -1,10 +1,10 @@
 <template>
   <section id="education">
     <h1>Educacion</h1>
-    <h2><img class="imagemedium"
-             src="../../static/usach_logo.png">
-      Universidad de Santiago de Chile - USACH</h2>
-    <p>Ingeniería en Computación e Informática, 2019 (Egresado)</p>
+    <h2><img class="imagemedium" src="../../static/usach_logo.png">Universidad de Santiago de Chile - USACH</h2>
+    <p>Ingeniería en Computación e Informática, 2019 (Egresado)
+      <a href="https://drive.google.com/file/d/1b6DSbaj92LpTQT2013JagXK8f2UkXZlJ/view?usp=sharing" target="_blank">[Memoria]</a>
+    </p>
     <h2><img class="imagemedium" src="../../static/utem_logo.png">
       Universidad Tecnologica Metropolitana - UTEM</h2>
     <p>Ingeniería en Informática, 2014 (No finalizado)</p>
