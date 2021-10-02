64d427c0f5522e7ef6892a46bcc1738113eed188
Esteban Plaza <esteban@vinken.cl>
2021-10-02 16:05:19 -0300

    ADD: redefine Hobbies.vue

--- src/components/Hobbies.vue
+++ src/components/Hobbies.vue
@@ -3,8 +3,8 @@
     <h1>Hobbies &amp; Intereses</h1>
     <ul ID="left-align">
       <li>Amo la lectura, principalmente de historia, finanzas y TI</li>
-      <li>Cocina 👨‍🍳</li>
-      <li>CounterStrike GO, Agregame en <img alt="" class="icon" src="../../static/steam_logo.png"/> @ad1cted .
+      <li>Chef Amateur 👨‍🍳</li>
+      <li>CounterStrike GO, @ad1cted on <img alt="" class="icon" src="../../static/steam_logo.png"/>
       </li>
     </ul>
   </section>
